5769839e9b028db314198e421fd2983b8202c9ad
my-first-repo <saiyadul77@gmail.com>
2022-04-22 11:59:31 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadCountries></LoadCountries>
     </div>
   );
+}
+function LoadCountries() {
+  const [countries, setCountries] = useState([])
+  useEffect(countries => {
+    fetch('https://restcountries.com/v2/all')
+      .then(res => res.json())
+      .then(data => setCountries(data))
+  }, [])
+  return (
+    <div>
+      <h1>Visiting every country in the World!!</h1>
+      <h4>Available Countries: {countries.length}</h4>
+      {
+        countries.map(country => <CountryName name={country.name} area={country.area} capital={country.capital} region={country.region}></CountryName>)
+      }
+    </div>
+  )
+}
+function CountryName(props) {
+  return (
+    <div className='country'>
+      <h1>Country Name:{props.name}</h1>
+      <h1>Total Area: {props.area}</h1>
+      <h1>Capital: {props.capital}</h1>
+      <h1>Region: {props.region}</h1>
+    </div>
+  )
 }
 
 export default App;
